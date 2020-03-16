7250306919f2e43da8e25ab08939b55e15ee1cca
Sean Hellum <seanhellum45@gmail.com>
2020-03-16 00:54:13 +0000

    Specify types

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,11 @@
 import * as vscode from "vscode";
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext): void {
   context.subscriptions.push(
     vscode.commands.registerCommand("extension.gpinit", () => {
       if (vscode.window.activeTerminal) {
         vscode.window.activeTerminal.sendText("gp init");
       } else {
-        const terminal = vscode.window.createTerminal();
+        const terminal: vscode.Terminal = vscode.window.createTerminal();
         terminal.sendText("gp init");
         terminal.show(true);
       }
