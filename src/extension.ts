cb6c5326718115cbeda97d68a74ce3371a2fd24b
Sean Hellum <seanhellum45@gmail.com>
2020-03-16 01:01:05 +0000

    add prettier config

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,15 @@
-import * as vscode from "vscode";
+import * as vscode from 'vscode'
 export function activate(context: vscode.ExtensionContext): void {
-  context.subscriptions.push(
-    vscode.commands.registerCommand("extension.gpinit", () => {
-      if (vscode.window.activeTerminal) {
-        vscode.window.activeTerminal.sendText("gp init");
-      } else {
-        const terminal: vscode.Terminal = vscode.window.createTerminal();
-        terminal.sendText("gp init");
-        terminal.show(true);
-      }
-    })
-  );
+    context.subscriptions.push(
+        vscode.commands.registerCommand('extension.gpinit', () => {
+            if (vscode.window.activeTerminal) {
+                vscode.window.activeTerminal.sendText('gp init')
+            } else {
+                const terminal: vscode.Terminal = vscode.window.createTerminal()
+                terminal.sendText('gp init')
+                terminal.show(true)
+            }
+        })
+    )
 }
 export function deactivate() {}
